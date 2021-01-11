ff7eee501271bb65f80681893a839546559c23e3
maalik0786 <54114655+maalik0786@users.noreply.github.com>
2021-01-11 20:48:04 +0100

    Update to net5.0

--- FastYolo/Model/ColorImage.cs
+++ FastYolo/Model/ColorImage.cs
@@ -8,7 +8,6 @@
 	/// </summary>
 	public class ColorImage
 	{
-		//public ColorImage(in int width, in int height) : this(new Size(width, height)) {}
 
 		public ColorImage(in Size size)
 		{
@@ -20,48 +19,5 @@
 		public Color[] Colors { get; set; }
 		public int Width => (int)Size.Width;
 		public int Height => (int)Size.Height;
-
-		/*public ColorImage(Size size, Color[] colors)
-		{
-			Size = size;
-			Colors = colors;
-			if (colors.Length != Width * Height)
-				throw new InvalidNumberOfColorsMustMatchWidthTimesHeight();
-		}
-
-		public class InvalidNumberOfColorsMustMatchWidthTimesHeight : Exception { }
-
-		public void Resize(in Size newSize)
-		{
-			if (newSize.Width <= Width && newSize.Height <= Height)
-				ResizeToSmaller(newSize);
-			else
-				ResizeToBigger(newSize);
-		}
-
-		private void ResizeToSmaller(in Size newSize)
-		{
-			var oldWidth = Width;
-			var oldHeight = Height;
-			Size = newSize;
-			for (int y=0; y<Height; y++)
-			for (int x = 0; x < Width; x++)
-				Colors[y * Width + x] = Colors[
-					(int)((y / newSize.Height) * oldHeight) * oldWidth +
-					(int)((x / newSize.Width) * oldWidth)];
-		}
-
-		private void ResizeToBigger(in Size newSize)
-		{
-			var oldWidth = Width;
-			var oldHeight = Height;
-			var oldColors = Colors;
-			Size = newSize;
-			Colors = new Color[Width * Height];
-			for (int y=0; y<Height; y++)
-			for (int x = 0; x < Width; x++)
-				Colors[y * Width + x] = oldColors[
-					(int)(y / newSize.Height * oldHeight) * oldWidth + (int)(x / newSize.Width * oldWidth)];
-		}*/
 	}
 }
--- FastYolo/Model/Size.cs
+++ FastYolo/Model/Size.cs
@@ -15,7 +15,6 @@ namespace FastYolo.Model
 	[Serializable]
 	public readonly struct Size : IEquatable<Size>
 	{
-		//public Size(float widthAndHeight) : this(widthAndHeight, widthAndHeight) { }
 
 		public Size(float width, float height)
 		{
@@ -23,68 +22,11 @@ namespace FastYolo.Model
 			Height = height;
 		}
 
-		/*public static readonly Size Zero;
-		public static readonly Size One = new Size(1, 1);
-		public static readonly Size Half = new Size(0.5f, 0.5f);
-		public static readonly Size Quarter = new Size(0.25f, 0.25f);
-		public static readonly Size Unused = new Size(-1, -1);
-		public static readonly int SizeInBytes = Marshal.SizeOf(typeof(Size));*/
-
 		[Pure]
 		public float Width { get; }
 		[Pure]
 		public float Height { get; }
 		
-		/*[Pure]
-		public float AspectRatio => Width / Height;
-
-		[Pure]
-		public static Size operator *(Size s1, Size s2)
-			=> new Size(s1.Width * s2.Width, s1.Height * s2.Height);
-
-		[Pure]
-		public static Size operator *(Size s, float f) => new Size(s.Width * f, s.Height * f);
-
-		[Pure]
-		public static Size operator *(float f, Size s) => new Size(f * s.Width, f * s.Height);
-
-		[Pure]
-		public static Size operator /(Size s, float f) => new Size(s.Width / f, s.Height / f);
-
-		[Pure]
-		public static Size operator /(float f, Size s) => new Size(f / s.Width, f / s.Height);
-
-		[Pure]
-		public static Size operator /(Size s1, Size s2)
-			=> new Size(s1.Width / s2.Width, s1.Height / s2.Height);
-
-		[Pure]
-		public static Size operator +(Size s1, Size s2)
-			=> new Size(s1.Width + s2.Width, s1.Height + s2.Height);
-
-		[Pure]
-		public static Size operator -(Size s1, Size s2)
-			=> new Size(s1.Width - s2.Width, s1.Height - s2.Height);*/
-
 		public bool Equals(Size other) => throw new NotImplementedException(); //ncrunch: no coverage 
-
-		/*[Pure]
-		public override bool Equals(object? other)
-			=> other is Size size ? Equals(size) : base.Equals(other);
-
-		[Pure]
-		public static bool operator ==(Size s1, Size s2) => s1.Equals(s2);
-
-		[Pure]
-		public static bool operator !=(Size s1, Size s2) => !s1.Equals(s2);
-
-		[Pure]
-		public override int GetHashCode()
-		{
-			unchecked
-			{
-				return Width.GetHashCode() * 31 + Height.GetHashCode();
-			}
-		}*/
 	}
 }
