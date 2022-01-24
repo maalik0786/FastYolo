df27e2d725fd7a9a1f8fc353689f572a17c10133
maalik0786 <54114655+maalik0786@users.noreply.github.com>
2022-01-24 16:30:10 +0100

    minor refactoring

--- FastYolo/DrawSquare.cs
+++ FastYolo/DrawSquare.cs
@@ -7,48 +7,48 @@ public sealed class DrawSquare
 	private static Color drawColor = Color.Red;
 	public void SetColor(Color color) => drawColor = color;
 
-	public static void DrawBoundingBox(ColorImage colorData, IEnumerable<YoloItem> items)
+	public static void DrawBoundingBox(ColorImage colorImage, IEnumerable<YoloItem> items)
 	{
 		foreach (var item in items)
-			DrawObjectFrame(colorData, item.X, item.Y, item.Width, item.Height);
+			DrawObjectFrame(colorImage, item.X, item.Y, item.Width, item.Height);
 	}
 
 	// ReSharper disable once TooManyArguments
-	public static void DrawObjectFrame(ColorImage vd, int x, int y, int width,
+	public static void DrawObjectFrame(ColorImage colorImage, int x, int y, int width,
 		int height)
 	{
 		if (x == 0 || y == 0 || width == 0 || height == 0)
 			return;
-		DrawHorizontalLine(vd, x, y, width);
-		DrawHorizontalLine(vd, x, y + height, width);
-		DrawVerticalLine(vd, x, y, height);
-		DrawVerticalLine(vd, x + width, y, height);
+		DrawHorizontalLine(colorImage, x, y, width);
+		DrawHorizontalLine(colorImage, x, y + height, width);
+		DrawVerticalLine(colorImage, x, y, height);
+		DrawVerticalLine(colorImage, x + width, y, height);
 	}
 
-	private static void DrawHorizontalLine(ColorImage vd, int x, int y, int width)
+	private static void DrawHorizontalLine(ColorImage colorImage, int x, int y, int width)
 	{
-		var colorPos = x + y * vd.Width;
+		var colorPos = x + y * colorImage.Width;
 		if (colorPos < 0)
 			colorPos = 0; //ncrunch: no coverage end
 		var stopBytePos = colorPos + width;
-		if (stopBytePos > vd.Colors.Length)
-			stopBytePos = vd.Colors.Length; //ncrunch: no coverage end
+		if (stopBytePos > colorImage.Colors.Length)
+			stopBytePos = colorImage.Colors.Length; //ncrunch: no coverage end
 		while (colorPos < stopBytePos)
-			vd.Colors[colorPos++] = drawColor;
+			colorImage.Colors[colorPos++] = drawColor;
 	}
 
-	private static void DrawVerticalLine(ColorImage vd, int x, int y, int height)
+	private static void DrawVerticalLine(ColorImage colorImage, int x, int y, int height)
 	{
-		var colorPos = x + y * vd.Width;
+		var colorPos = x + y * colorImage.Width;
 		if (colorPos < 0)
 			colorPos = 0; //ncrunch: no coverage end
-		var stopBytePos = colorPos + height * vd.Width;
-		if (stopBytePos > vd.Colors.Length)
-			stopBytePos = vd.Colors.Length; //ncrunch: no coverage end
+		var stopBytePos = colorPos + height * colorImage.Width;
+		if (stopBytePos > colorImage.Colors.Length)
+			stopBytePos = colorImage.Colors.Length; //ncrunch: no coverage end
 		while (colorPos < stopBytePos)
 		{
-			vd.Colors[colorPos++] = drawColor;
-			colorPos += vd.Width - 1;
+			colorImage.Colors[colorPos++] = drawColor;
+			colorPos += colorImage.Width - 1;
 		}
 	}
 }
